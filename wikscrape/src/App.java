e31520331c31fe2a2bdc008c73a28371e9007a9d
Nicholas DeMaestri <demaesm555@gmail.com>
2024-12-27 22:31:05 -0500

    bugfix

--- wikscrape/src/App.java
+++ wikscrape/src/App.java
@@ -41,7 +41,7 @@ public class App
                 String[] prev_name = {""};
                 int date_type = 0; //0 for FULL, 1 for YEAR
                 String[] prev_date = {"000000009999-99-99-0000"}; //Format used by default when date is FULL formatted as ex. "November 18th, 1984"
-                if(!(doc.select("table tr:eq(0) td:eq(2)").text().equals(""))) //Checks if date format instead is simply just YEAR (ex. 1984)
+                if(!(doc.select("table tr:eq(1) td:eq(2)").text().equals(""))) //Checks if date format instead is simply just YEAR (ex. 1984)
                     {
                         prev_date[0] = "9999";
                         date_type = 1;

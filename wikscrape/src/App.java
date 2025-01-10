53aa21fc65dd308aed469ba348144fb7d3f8d8d6
Nicholas DeMaestri <demaesm555@gmail.com>
2025-01-10 17:56:27 -0500

    Updated Program
    Updated to reflect the further improvements made and pulled into the main branch

--- wikscrape/src/App.java
+++ wikscrape/src/App.java
@@ -1,4 +1,3 @@
-//When modifying use: https://jsoup.org/apidocs/org/jsoup/select/Selector.html
 /* 
 Note for future self: Each group of articles for whatever it may be that you are trying to
                       sort is formatted slightly differently, modifications to the program
@@ -6,6 +5,8 @@ Note for future self: Each group of articles for whatever it may be that you are
                       even make some major generalization fixes or bug fixes while you are
                       at it, such as I did when writing this.
 */
+
+
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -23,11 +24,19 @@ public class App
 
         //List of URLs to parse tables of (change to what is needed)
         String[] URLs = {"https://en.wikipedia.org/wiki/List_of_arcade_video_games:_0%E2%80%939", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_A", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_B", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_C",
-                        "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_D", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_E", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_F", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_G", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_H", 
-                        "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_I", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_J", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_K", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_L", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_M", 
-                        "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_N", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_O", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_P", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_Q", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_R", 
-                        "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_S", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_T", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_U", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_V", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_W", 
-                        "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_X", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_Y", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_Z"};
+        "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_D", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_E", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_F", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_G", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_H", 
+        "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_I", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_J", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_K", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_L", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_M", 
+        "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_N", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_O", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_P", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_Q", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_R", 
+        "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_S", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_T", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_U", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_V", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_W", 
+        "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_X", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_Y", "https://en.wikipedia.org/wiki/List_of_arcade_video_games:_Z"};
+
+            /* Pretty much everything else you will need to modify with
+            some rare exceptions, use this link as a guide: https://jsoup.org/apidocs/org/jsoup/select/Selector.html
+        /*--------------------------------------------------------------------*/
+        /* */       String titleLoc = "td:eq(0)";                             // 
+        /* */       String dateLoc = "td:eq(2)";                              // 
+        /* */       String systemLoc = "td:eq(3)";                            // 
+        /*--------------------------------------------------------------------*/ 
 
         //List of stored games, to be sorted by release date.
         Map<String, String> games = new TreeMap<>();
@@ -43,43 +52,48 @@ public class App
                 //Vars needed for storing info for future comparisons
                 String prevName = "";
                 String[] prev_name = {""};
-                int date_type = 0; //0 for FULL, 1 for YEAR
-                String[] prev_date = {"000000009999-99-99-0000"}; //Format used by default when date is FULL formatted as ex. "November 18th, 1984"
-                if(!(doc.select("table tr:eq(1) td:eq(2)").text().equals(""))) //Checks if date format instead is simply just YEAR (ex. 1984)
+                int date_type = 1; //0 for FULL, 1 for YEAR
+                String[] prev_date = {"9999"}; //Format used by default when date is YEAR formatted as ex. "1984"
+                if(!(doc.select("table tr:eq(1) " + dateLoc).attr("data-sort-value").equals(""))) //Checks if date format instead is the FULL date (ex. "November 18th, 1984")
                     {
-                        prev_date[0] = "9999";
-                        date_type = 1;
+                        prev_date[0] = "000000009999-99-99-0000";
+                        date_type = 0;
                     }
 
                 String[] disp_date = new String[1];
-                String[] developer = new String[1];
+                String[] system = new String[1];
 
                 //Iterate over each row in the table
                 for (Element row: doc.select("table tr"))
                     {
-                        //System.out.println(row.select("td:eq(2)").text());
+                        //System.out.println(row.select(dateLoc).text());
 
                         //Get relevant info for each title
-                        String title = row.select("td:eq(0) i a").text();
-                        if(title.equals(""))
-                        title = row.select("td:eq(0) i").text(); //Handles titles with no hyperlinks
+                        String titleTextLoc = titleLoc + " i a"; //Gets the exact location of where the text is (can vary due to some entries missing hyperlinks)
+                        String title = row.select(titleTextLoc).text();
+                        if(title.equals("")) //Handles titles with no hyperlinks
+                        {
+                            titleTextLoc = titleLoc + " i";
+                            title = row.select(titleTextLoc).text();
+                        }
+                        
 
                         String date = "";
                         switch(date_type)
                         {
                             case 0:
-                            date = row.select("td span[data-sort-value]").attr("data-sort-value");
-                            disp_date[0] = row.select("td span[data-sort-value]").text();
+                            date = row.select(dateLoc).attr("data-sort-value");
+                            disp_date[0] = row.select(dateLoc).text();
                             break;
                             
                             case 1:
-                            date = row.select("td:eq(2)").text();
+                            date = row.select(dateLoc).text();
                             disp_date[0] = date;
                             break;
                         }
                         
                         if(!(date.equals(""))) {prev_date[0] = date;}
-                        developer[0] = row.select("td:eq(3)").text();
+                        system[0] = row.select(systemLoc).text();
 
                         //For any rows with blank titles, uses the last non-blank title
                         if(!(title.equals("")))
@@ -88,14 +102,14 @@ public class App
 
                                 //For each title, scan over every date to determine which version came out first (we want to play versions for the original respective platforms)
                                 Element next = row.nextElementSibling();
-                                while(next != null && (next.select("td i a").text().equals("") || next.select("td i a").text().equals(prev_name[0])))
+                                while(next != null && (next.select(titleTextLoc).text().equals("") || next.select(titleTextLoc).text().equals(prev_name[0])))
                                 {
-                                    //System.out.println(next.select("td span[data-sort-value]").attr("data-sort-value"));
-                                    if(!(next.select("td span[data-sort-value]").attr("data-sort-value").equals("")) && next.select("td span[data-sort-value]").attr("data-sort-value").compareTo(prev_date[0]) < 0)
+                                    //System.out.println(next.select(dateLoc).attr("data-sort-value"));
+                                    if(!(next.select(dateLoc).attr("data-sort-value").equals("")) && next.select(dateLoc).attr("data-sort-value").compareTo(prev_date[0]) < 0)
                                     {
-                                        prev_date[0] = next.select("td span[data-sort-value]").attr("data-sort-value");
-                                        disp_date[0] = next.select("td span[data-sort-value]").text();
-                                        developer[0] = next.select("td:eq(3)").text();
+                                        prev_date[0] = next.select(dateLoc).attr("data-sort-value");
+                                        disp_date[0] = next.select(dateLoc).text();
+                                        system[0] = next.select(systemLoc).text();
                                     }
 
                                     next = next.nextElementSibling();
@@ -119,7 +133,7 @@ public class App
                                 {date=date+"1";}
 
                                 //Add data together then add it to the map to be sorted
-                                title = prevName + " - "+disp_date[0]+": "+developer[0];
+                                title = prevName + " - "+disp_date[0]+": "+system[0];
                                 //System.out.println(title);
                                 games.put(date, title);
 
